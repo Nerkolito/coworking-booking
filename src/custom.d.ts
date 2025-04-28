66c1c13b4dc3b014013b690e460742dda60b5629
Nerkolito <nerminkruskic@gmail.com>
2025-04-28 08:39:24 +0200

    Comment fix

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,7 +1,7 @@
 // Importerar typen JwtPayload som vi själva har definierat i authMiddleware
 import { JwtPayload } from "./middleware/authMiddleware";
 
-// Utökar Express Request-objektet så att vi kan lägga till "user" direkt på req.user
+// Utökar Express Request-objektet så att man kan lägga till "user" direkt på req.user
 declare global {
   namespace Express {
     interface Request {
